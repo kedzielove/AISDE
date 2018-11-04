361158fc290e8489967eb17676f897a1b84f2ecc
Hubert Zajączkowski <33200790+Hughevre@users.noreply.github.com>
2018-11-04 15:16:31 +0100

    Edge class extracted

---
+++ Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISDE
+{
+    class Edge
+    {
+        public Node NodeA { get; private set; }
+        public Node NodeB { get; private set; } 
+        public Edge(Node nodeA, Node nodeB)
+        {
+            NodeA = nodeA;
+            NodeB = nodeB;
+        }
+
+        public double Weight
+        {
+            get => Math.Sqrt(Math.Pow(NodeA.X - NodeB.X, 2) + (Math.Pow(NodeA.Y - NodeB.Y, 2)));
+        }
+    }
+}
